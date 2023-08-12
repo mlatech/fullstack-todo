6044e8dfdd0b26dfb6bda4a57fb4753a73b99054
Karyna Chernyak <mlatech@icloud.com>
2023-08-11 22:19:41 -0500

    .

--- server/src/router.js
+++ server/src/router.js
@@ -5,7 +5,5 @@ const router = express.Router()
 
 router.post('/login', require('./routes/loginRoute'))
 
-
-
 router.get('/todos', isLoggedIn, require('./routes/todosRoute'))
 module.exports = router
